1602902cacf564bfdc7c0fd815a638dde1cb3111
tahsinabrar <abrar.cste@gmail.com>
2014-07-09 12:40:06 +0600

    Unary Operator practicing & precedence testing

--- operators.php
+++ operators.php
@@ -3,7 +3,7 @@
 $x=100/(2*5);
 $y=100%(2*5);
 $z=100/ 2 * 5;
-$a=0 / 5;
+$a=0 / 5; 
 $b=0 % 5;
 
 echo $x."<br/>"; //10
@@ -11,4 +11,27 @@ echo $y."<br/>"; //0
 echo $z."<br/>"; //250
 echo $a."<br/>"; //0
 echo $b."<br/>"; //0
+
+//Unary Operators
+$a=2;
+echo $a++."<br/>"; //2
+echo $a--."<br/>"; //3
+echo ++$a."<br/>"; //3
+echo --$a."<br/>"; //2
+echo $a.PHP_EOL; //2
+echo ++$a;
+
+$a=4;
+$b=2;
+echo ++$a*($a++ + --$b);
+/*
+* First execution starts with first bracket. 
+* Initial value of a is 4 and b is 2.
+* So, at first echo ++$a*(4+1);
+* Then $a will be incremented because $a++;
+* Then  $a will again be pre-incremented in the statement.
+* Now echo 6*(4+1);
+* So, the result is 30.
+*/
+
 ?>
